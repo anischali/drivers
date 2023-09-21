63a50b72ce4e0d988cf1dabe45aeb2b549012cbd
Anis Chali <anis.chali@exfo.com>
2023-09-21 13:35:54 -0400

    hid-battery: implement auto detect battery properties.

--- hid-composite/hid-battery.c
+++ hid-composite/hid-battery.c
@@ -13,11 +13,22 @@
 #include "hid_composite.h"
 
 
+enum battery_usages {
+	HID_BATTERY_USAGE = 0x850000U,
+	HID_BATTERY_BATTERY_PRESENT = 0x8500D1U,
+	HID_BATTERY_POWER_FAIL = 0x8500D2U,
+	HID_BATTERY_ALARM_INHIBITED = 0x8500D3U,
+	HID_BATTERY_THERMISTOR_HOT = 0x8500D5U,
+	HID_BATTERY_THERMISTOR_COLD = 0x8500D6U,
+	HID_BATTERY_VOLTAGE_OUT_OF_RANGE = 0x8500D8U,
+	HID_BATTERY_CURRENT_OUT_OF_RANGE = 0x8500D9U,
+};
+
+
 struct hid_battery {
 	struct hid_subdevice *hsdev;
 	struct power_supply_desc desc;
 	struct power_supply *battery;
-	struct power_supply_battery_info *info;
 	struct hid_composite_callbacks callbacks;
 };
 
@@ -26,42 +37,90 @@ static int hid_battery_get_property(struct power_supply *psy,
 					union power_supply_propval *val)
 {
 	struct hid_battery *bat = power_supply_get_drvdata(psy);
-	struct power_supply_battery_info *info = bat->info;
-	int ret;
-
+	
+	val->intval = 0;
 	switch (psp) {
+	case POWER_SUPPLY_PROP_STATUS:
+		val->intval = POWER_SUPPLY_STATUS_FULL;
+		break;
 	case POWER_SUPPLY_PROP_HEALTH:
-		//ret = iio_read_channel_processed(bat->channel, &val->intval);
-		val->intval *= 1000;
-		if (val->intval < info->voltage_min_design_uv)
-			val->intval = POWER_SUPPLY_HEALTH_DEAD;
-		else if (val->intval > info->voltage_max_design_uv)
-			val->intval = POWER_SUPPLY_HEALTH_OVERVOLTAGE;
-		else
-			val->intval = POWER_SUPPLY_HEALTH_GOOD;
-		return ret;
+		break;
+	case POWER_SUPPLY_PROP_PRESENT:
+		val->intval = 1;
+		break;
+	case POWER_SUPPLY_PROP_TECHNOLOGY:
+		break;
+	case POWER_SUPPLY_PROP_VOLTAGE_MIN_DESIGN: /* THE design voltage... */
+		break;
 	case POWER_SUPPLY_PROP_VOLTAGE_NOW:
-		//ret = iio_read_channel_processed(bat->channel, &val->intval);
-		val->intval *= 1000;
-		return ret;
-	case POWER_SUPPLY_PROP_VOLTAGE_MIN_DESIGN:
-		val->intval = info->voltage_min_design_uv;
-		return 0;
-	case POWER_SUPPLY_PROP_VOLTAGE_MAX_DESIGN:
-		val->intval = info->voltage_max_design_uv;
-		return 0;
+		break;
+	case POWER_SUPPLY_PROP_CURRENT_NOW:
+		break;
+	case POWER_SUPPLY_PROP_CURRENT_AVG:
+		break;
+	case POWER_SUPPLY_PROP_POWER_NOW:
+		break;
+	case POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN:
+		break;
+	case POWER_SUPPLY_PROP_CHARGE_NOW:
+		break;
+	case POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT:
+		break;
+	case POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT_MAX:
+		break;
+	case POWER_SUPPLY_PROP_CAPACITY:
+		break;
+	case POWER_SUPPLY_PROP_CAPACITY_LEVEL:
+		break;
+	case POWER_SUPPLY_PROP_TEMP:
+		break;
+	case POWER_SUPPLY_PROP_TEMP_AMBIENT:
+		break;
+	case POWER_SUPPLY_PROP_MODEL_NAME:
+		break;
+	case POWER_SUPPLY_PROP_MANUFACTURER:
+		break;
+	case POWER_SUPPLY_PROP_SERIAL_NUMBER:
+		break;
 	default:
-		return -EINVAL;
+		break;
 	}
+	return 0;
 }
 
 
-static enum power_supply_property hid_battery_properties[] = {
-	POWER_SUPPLY_PROP_HEALTH,
-	POWER_SUPPLY_PROP_VOLTAGE_NOW,
-	POWER_SUPPLY_PROP_VOLTAGE_MIN_DESIGN,
-	POWER_SUPPLY_PROP_VOLTAGE_MAX_DESIGN,
-};
+static int hid_battery_properties_count(struct hid_device *hdev)
+{
+	struct hid_report *report;
+	struct hid_field *field;
+	int i, j;
+	unsigned int count = 0;
+
+	list_for_each_entry(report,
+			    &hdev->report_enum[HID_INPUT_REPORT].report_list,
+			    list) {
+		for (i = 0; i < report->maxfield; i++) {
+			field = report->field[i];
+			if (field->application == HID_BATTERY_USAGE)
+			{
+				count += field->maxusage;
+				for (j = 0; j < field->maxusage; ++j)
+				{
+					hid_info(hdev, "usage: 0x%08x (size: %d count: %d)\n", field->usage[j].hid, field->report_size, field->report_size);
+				}
+			}
+		}
+	}
+
+	hid_info(hdev, "count: %d\n", count);
+	return count;
+}
+
+static void hid_battery_parse(struct hid_device *hdev, 
+					enum power_supply_property **properties)
+{
+
+}
 
 static int hid_battery_probe(struct platform_device *pdev)
 {
@@ -69,6 +128,7 @@ static int hid_battery_probe(struct platform_device *pdev)
 	struct hid_battery *bat;
 	struct power_supply_config psy_cfg = {};
 	struct power_supply_desc *desc;
+	enum power_supply_property *hid_battery_properties;
 	int ret;
 
 	if (!hsdev)
@@ -80,14 +140,7 @@ static int hid_battery_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, bat);
 
-	bat->hsdev = hsdev;
-	desc = &bat->desc;
-	desc->name = "hid-battery";
-	desc->type = POWER_SUPPLY_TYPE_BATTERY;
-	desc->properties = hid_battery_properties;
-	desc->num_properties = ARRAY_SIZE(hid_battery_properties);
-	desc->get_property = hid_battery_get_property;
-	psy_cfg.drv_data = bat;
+	
 	
 	ret = hid_composite_device_open(hsdev);
 	if (ret)
@@ -95,6 +148,27 @@ static int hid_battery_probe(struct platform_device *pdev)
 
 	hid_device_io_start(hsdev->hdev);
 
+	hid_battery_properties = devm_kcalloc(&pdev->dev, 2,
+				sizeof(enum power_supply_property), GFP_KERNEL);
+	if (!hid_battery_properties)
+	{
+		ret = -ENOMEM;
+		goto cleanup;
+	}
+
+	hid_battery_properties_count(hsdev->hdev);
+
+	hid_battery_properties[0] = POWER_SUPPLY_PROP_STATUS;
+	hid_battery_properties[0] = POWER_SUPPLY_PROP_PRESENT;
+
+	bat->hsdev = hsdev;
+	desc = &bat->desc;
+	desc->name = "hid-battery";
+	desc->type = POWER_SUPPLY_TYPE_BATTERY;
+	desc->properties = hid_battery_properties;
+	desc->num_properties = 2;
+	desc->get_property = hid_battery_get_property;
+	psy_cfg.drv_data = bat;
 	bat->battery = devm_power_supply_register(&pdev->dev, desc, &psy_cfg);
 	if (IS_ERR(bat->battery)) {
 		hid_device_io_stop(hsdev->hdev);
@@ -103,19 +177,10 @@ static int hid_battery_probe(struct platform_device *pdev)
 				     "Unable to register battery\n");
 
 	}
-	ret = power_supply_get_battery_info(bat->battery, &bat->info);
+	
+	ret = power_supply_powers(bat->battery, &pdev->dev);
 	if (ret) {
 		hid_err(hsdev->hdev, "Unable to get battery info: %d\n", ret);
-		goto cleanup;
-	}
-	if (bat->info->voltage_min_design_uv < 0) {
-		hid_err(hsdev->hdev, "Unable to get voltage min design\n");
-		ret = bat->info->voltage_min_design_uv;
-		goto cleanup;
-	}
-	if (bat->info->voltage_max_design_uv < 0) {
-		hid_err(hsdev->hdev, "Unable to get voltage max design\n");
-		ret = bat->info->voltage_max_design_uv;
 		goto cleanup;
 	}
 
@@ -140,7 +205,7 @@ static int hid_battery_remove(struct platform_device *pdev)
 static const struct platform_device_id hid_battery_platform_ids[] = {
 	{
 		/* Format: HID-COMPOSITE-usage_id_in_hex_lowercase */
-		.name = "HID-COMPOSITE-10070",
+		.name = "HID-COMPOSITE-850000",
 	},
 	{ /* sentinel */ }
 };
